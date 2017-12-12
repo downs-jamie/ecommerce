f5ade3fcfb68a0121e1205e6d5943571c738fd2c
downs19 <downs.jamie@gmail.com>
2017-12-12 10:10:15 -0500

    updated

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore, applyMiddleware } from 'redux';
 
-import registerServiceWorker from './registerServiceWorker';
-import Student from './Student';
+
+
+// import registerServiceWorker from './registerServiceWorker';
+// import Student from './Student';
+
+import RootReducer from './reducers/RootReducer';
+
+import reduxPromise from 'redux-promise'
 
 ReactDOM.render(
 	<App />,
